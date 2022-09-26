4f43c5fc4a8abb08c86a665261324c91ccdc415c
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-26 17:06:51 +0100

    Added first function, forgot to save the first time, please see first commit for details.

---
+++ run.py
@@ -0,0 +1,8 @@
+def get_user_name():
+    """
+    Gets user name.
+    """
+    print("Welcome to Minesweeper! Watch your step!")
+    name_str = input("Enter your name here: ")
+    print(f"Enjot the game {name_str}!  ")
+get_user_name()  
