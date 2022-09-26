351f4502ce5bbb4fb8b81e2d1c414b140f2bae23
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-26 17:28:22 +0100

    Added a parent class to more easily control the constructionn of the board

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
+class Game_board:
+    def __init__(self, bomb_num, board_sze):
+
+        self.bomb_num
+        self.board_size
+
 def get_user_name():
-    """
-    Gets user name.
-    """
-    print("Welcome to Minesweeper! Watch your step!")
-    name_str = input("Enter your name here: ")
-    print(f"Enjot the game {name_str}!  ")
+        """
+        Gets user name.
+        """
+        print("Welcome to Minesweeper! Watch your step!")
+        name_str = input("Enter your name here: ")
+    
+        print(f"Best of luck {name_str}!")
+
 get_user_name()  
