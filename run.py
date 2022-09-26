d0c64e9041d38e3cb4f44c19bfc2a9b86890a3d0
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-26 22:49:04 +0100

    trying to fix attribute error on line 75

--- run.py
+++ run.py
@@ -72,7 +72,7 @@ class Game:
         return bombs_beside
     
     def search(self, row, col):
-        self.search.add((row,col))
+        self.already_search.add((row,col))
         
         if self.game[row][col] == '*':
             return False
