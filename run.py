ef468abe1488bca9b104c2753854d48084f0e2a4
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-27 01:58:22 +0100

    fix len in while loop

--- run.py
+++ run.py
@@ -144,7 +144,7 @@ def play(board_dim=10, bomb_num=10):
      
     safe = True
      
-    while len(game.search) < game.board_dim ** 2 - bomb_num:
+    while len(game.already_search) < game.board_dim ** 2 - bomb_num:
         print(game)
         user_input = re.split (',(\\s)', input("Choose your dig site! Choose well! Format as row, col:"))
         row, col = int(user_input[0]), int(user_input[-1])
