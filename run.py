69f3d292abb2b3186e670ae6eebe551e30fb6e2d
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-09-29 02:04:51 +0100

    fin

--- run.py
+++ run.py
@@ -1,170 +1,340 @@
+# Importing packages
 import random
-import re
-
-def get_user_name():
-        """
-        Gets user name.
-        """
-        print("Welcome to Minesweeper! Watch your step!")
-        name_str = input("Enter your name here: ")
-    
-        print(f"Best of luck {name_str}!")
-        
-class Game:
-    def __init__(self, bomb_num, board_dim):
-   
-        self.bomb_num = bomb_num
-        self.board_dim = board_dim
-        """
-        The game  class allows us to easily replicate objects using OOP tools.
-        """
-        self.game = self.generate_new_game()
-        self.assign_values()
-        """
-        adding an empty set that we will use to store which locations we 
-        have already search for bombs using tuples (row, col)
-        """
-        self.already_search = ()        
-    def generate_new_game(self):
-        """
-        generates a game by defined dimenion size.
-        """
-        game = [[None for _ in range(self.board_dim)] for _ in range(self.board_dim)]
-        """
-        planting bombs by using an equation to return random intergers
-        """
-        bombs_plnted = 0
-        while bombs_plnted < self.bomb_num:
-            loc = random.randint(0, self.board_dim**2 - 1)
-            row = loc // self.board_dim
-            col = loc % self.board_dim
-            
-            if game[row][col] == '*':
-                #bomb already there so continue on with loop
-                continue
-           
-            game[row][col] == '*' #plants the bomb
-            bombs_plnted += 1
-        
-        return game    
-     
-    def assign_values(self):
-        for c in range(self.board_dim):
-            for r in range(self.board_dim):
-                if self.game[r][c] == '*':
-                    continue
-                self.game[r][c] = self.highlight_adjacent_bombs(r, c)
-    
-    def highlight_adjacent_bombs(self, row, col):
-        """
-         iterating through adjacent positions, making sure not to go out of bounds by using rhe min and max.
-        """  
-        bombs_beside = 0
-        for r in range (max(0, row-1), min(self.board_dim-1, row+1)+1):
-            for c in range(max(0, col-1), min(self.board_dim-1, col+1)+1):
-                if r == row and c == col:
-                    
-                    continue
-                if self.game[r][c] == '*':
-                    bombs_beside += 1
-    
-        return bombs_beside
-    
-    def search(self, row, col):
-        
-        self.already_search.add((row, col))
-        
-        if self.game[row][col] == '*':
-            return False
-        elif self.game[row][col] > 0:
-            return True
-        """
-        This function keeps track of what we have searched. Using repeat logic
-        """
-        for r in range (max(0, row-1), min(self.board_dim-1, row+1)+1):
-            for c in range(max(0, col-1), min(self.board_dim-1, col+1)+1):
-                if (r, c) in self.already_search:
-                    continue
-                self.already_search(r, c)
-        
-        return True
-        
-    def __str__(self):
-        """
-        The below code creates a new array that represents what the user will see.
-        """
-        shown_board = [[None for _ in range(self.board_dim)] for _ in range(self.board_dim)]
-        for row in range (self.board_dim):
-            for col in range(self.board_dim):
-                if (row, col) in self.already_search:
-                    shown_board[row][col] = str(self.game[row][col])
-                else:
-                    shown_board[row][col] = ' '
-                   
-        string_rep = ''
-                  
-        # get max column widths for printing
-        widths = []
-        for idx in range(self.board_dim):
-            columns = map(lambda x: x[idx], shown_board)
-            widths.append(
-                len(
-                    max(columns, key = len)
-                )
-            )
-
-        # printing the csv strings
-        indices = [i for i in range(self.board_dim)]
-        indices_row = '   '
-        cells = []
-        for idx, col in enumerate(indices):
-            format = '%-' + str(widths[idx]) + "s"
-            cells.append(format % (col))
-        indices_row += '  '.join(cells)
-        indices_row += '  \n'
-        
-        for i in range(len(shown_board)):
-            row = shown_board[i]
-            string_rep += f'{i} |'
-            cells = []
-            for idx, col in enumerate(row):
-                format = '%-' + str(widths[idx]) + "s"
-                cells.append(format % (col))
-            string_rep += ' |'.join(cells)
-            string_rep += ' |\n'
-
-        str_len = int(len(string_rep) / self.board_dim)
-        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
-
-        return string_rep
-               
-def play(board_dim=10, bomb_num=10):
-    game = Game(board_dim, bomb_num)
-     
-    safe = True
-     
-    while len(game.already_search) < game.board_dim ** 2 - bomb_num:
-        print(game)
-        user_input = re.split (',(\\s)', input("Choose your dig site! Choose well! Format as row, col:"))
-        row, col = int(user_input[0]), int(user_input[-1])
-        if row < 0 or row >= board_dim or col < 0 or col >= board_dim:
-            print("Ooopsie, Try Again!")
-            continue
-        safe = game.search(row, col)
-        if not safe:
-            break
-        
-    if safe:
-        print("You did it!! Well done :)")
-    else:
-        print("GAME OVER!!! :( ")
-        game.search = [(r,c) for r in range(game.board_dim) for c in range(game.board_dim)]
-        print(game)
-    print(game)
-    
-def main():
+import os
  
-    get_user_name()  
-    play()
-    
-main()
+# Printing the Minesweeper Layout
+def print_mines_layout():
+ 
+    global mine_values
+    global n
+ 
+    print()
+    print("\t\t\tMINESWEEPER\n")
+ 
+    st = "   "
+    for i in range(n):
+        st = st + "     " + str(i + 1)
+    print(st)   
+ 
+    for r in range(n):
+        st = "     "
+        if r == 0:
+            for col in range(n):
+                st = st + "______" 
+            print(st)
+ 
+        st = "     "
+        for col in range(n):
+            st = st + "|     "
+        print(st + "|")
+         
+        st = "  " + str(r + 1) + "  "
+        for col in range(n):
+            st = st + "|  " + str(mine_values[r][col]) + "  "
+        print(st + "|") 
+ 
+        st = "     "
+        for col in range(n):
+            st = st + "|_____"
+        print(st + '|')
+ 
+    print()
+  
+# Function for setting up Mines
+def set_mines():
+ 
+    global numbers
+    global mines_no
+    global n
+ 
+    # Track of number of mines already set up
+    count = 0
+    while count < mines_no:
+ 
+        # Random number from all possible grid positions 
+        val = random.randint(0, n*n-1)
+ 
+        # Generating row and column from the number
+        r = val // n
+        col = val % n
+ 
+        # Place the mine, if it doesn't already have one
+        if numbers[r][col] != -1:
+            count = count + 1
+            numbers[r][col] = -1
+ 
+# Function for setting up the other grid values
+def set_values():
+ 
+    global numbers
+    global n
+ 
+    # Loop for counting each cell value
+    for r in range(n):
+        for col in range(n):
+ 
+            # Skip, if it contains a mine
+            if numbers[r][col] == -1:
+                continue
+ 
+            # Check up  
+            if r > 0 and numbers[r-1][col] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check down    
+            if r < n-1  and numbers[r+1][col] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check left
+            if col > 0 and numbers[r][col-1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check right
+            if col < n-1 and numbers[r][col+1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check top-left    
+            if r > 0 and col > 0 and numbers[r-1][col-1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check top-right
+            if r > 0 and col < n-1 and numbers[r-1][col+1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check below-left  
+            if r < n-1 and col > 0 and numbers[r+1][col-1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+            # Check below-right
+            if r < n-1 and col < n-1 and numbers[r+1][col+1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
+ 
+# Recursive function to display all zero-valued neighbours  
+def neighbours(r, col):
+     
+    global mine_values
+    global numbers
+    global vis
+ 
+    # If the cell already not visited
+    if [r,col] not in vis:
+ 
+        # Mark the cell visited
+        vis.append([r,col])
+ 
+        # If the cell is zero-valued
+        if numbers[r][col] == 0:
+ 
+            # Display it to the user
+            mine_values[r][col] = numbers[r][col]
+ 
+            # Recursive calls for the neighbouring cells
+            if r > 0:
+                neighbours(r-1, col)
+            if r < n-1:
+                neighbours(r+1, col)
+            if col > 0:
+                neighbours(r, col-1)
+            if col < n-1:
+                neighbours(r, col+1)    
+            if r > 0 and col > 0:
+                neighbours(r-1, col-1)
+            if r > 0 and col < n-1:
+                neighbours(r-1, col+1)
+            if r < n-1 and col > 0:
+                neighbours(r+1, col-1)
+            if r < n-1 and col < n-1:
+                neighbours(r+1, col+1)  
+ 
+        # If the cell is not zero-valued            
+        if numbers[r][col] != 0:
+                mine_values[r][col] = numbers[r][col]
+ 
+# Function for clearing the terminal
+def clear():
+    os.system("clear")      
+ 
+# Function to display the instructions
+def instructions():
+    print("Instructions:")
+    print("1. Enter row and column number to select a cell, Example \"2 3\"")
+    print("2. In order to flag a mine, enter F after row and column numbers, Example \"2 3 F\"")
+ 
+# Function to check for completion of the game
+def check_over():
+    global mine_values
+    global n
+    global mines_no
+ 
+    # Count of all numbered values
+    count = 0
+ 
+    # Loop for checking each cell in the grid
+    for r in range(n):
+        for col in range(n):
+ 
+            # If cell not empty or flagged
+            if mine_values[r][col] != ' ' and mine_values[r][col] != 'F':
+                count = count + 1
+     
+    # Count comparison          
+    if count == n * n - mines_no:
+        return True
+    else:
+        return False
+ 
+# Display all the mine locations                    
+def show_mines():
+    global mine_values
+    global numbers
+    global n
+ 
+    for r in range(n):
+        for col in range(n):
+            if numbers[r][col] == -1:
+                mine_values[r][col] = 'M'
+ 
+ 
+if __name__ == "__main__":
+ 
+    # Size of grid
+    n = 8
+    # Number of mines
+    mines_no = 8
+ 
+    # The actual values of the grid
+    numbers = [[0 for y in range(n)] for x in range(n)] 
+    # The apparent values of the grid
+    mine_values = [[' ' for y in range(n)] for x in range(n)]
+    # The positions that have been flagged
+    flags = []
+ 
+    # Set the mines
+    set_mines()
+ 
+    # Set the values
+    set_values()
+ 
+    # Display the instructions
+    instructions()
+ 
+    # Variable for maintaining Game Loop
+    over = False
+         
+    # The GAME LOOP 
+    while not over:
+        print_mines_layout()
+ 
+        # Input from the user
+        inp = input("Enter row number followed by space and column number = ").split()
+         
+        # Standard input
+        if len(inp) == 2:
+ 
+            # Try block to handle errant input
+            try: 
+                val = list(map(int, inp))
+            except ValueError:
+                clear()
+                print("Wrong input!")
+                instructions()
+                continue
+ 
+        # Flag input
+        elif len(inp) == 3:
+            if inp[2] != 'F' and inp[2] != 'f':
+                clear()
+                print("Wrong Input!")
+                instructions()
+                continue
+ 
+            # Try block to handle errant input  
+            try:
+                val = list(map(int, inp[:2]))
+            except ValueError:
+                clear()
+                print("Wrong input!")
+                instructions()
+                continue
+ 
+            # Sanity checks 
+            if val[0] > n or val[0] < 1 or val[1] > n or val[1] < 1:
+                clear()
+                print("Wrong input!")
+                instructions()
+                continue
+ 
+            # Get row and column numbers
+            r = val[0]-1
+            col = val[1]-1 
+ 
+            # If cell already been flagged
+            if [r, col] in flags:
+                clear()
+                print("Flag already set")
+                continue
+ 
+            # If cell already been displayed
+            if mine_values[r][col] != ' ':
+                clear()
+                print("Value already known")
+                continue
+ 
+            # Check the number for flags    
+            if len(flags) < mines_no:
+                clear()
+                print("Flag set")
+ 
+                # Adding flag to the list
+                flags.append([r, col])
+                 
+                # Set the flag for display
+                mine_values[r][col] = 'F'
+                continue
+            else:
+                clear()
+                print("Flags finished")
+                continue    
+ 
+        else: 
+            clear()
+            print("Wrong input!")   
+            instructions()
+            continue
+             
+ 
+        # Sanity checks
+        if val[0] > n or val[0] < 1 or val[1] > n or val[1] < 1:
+            clear()
+            print("Wrong Input!")
+            instructions()
+            continue
+             
+        # Get row and column number
+        r = val[0]-1
+        col = val[1]-1
+ 
+        # Unflag the cell if already flagged
+        if [r, col] in flags:
+            flags.remove([r, col])
+ 
+        # If landing on a mine --- GAME OVER    
+        if numbers[r][col] == -1:
+            mine_values[r][col] = 'M'
+            show_mines()
+            print_mines_layout()
+            print("Landed on a mine. GAME OVER!!!!!")
+            over = True
+            continue
+ 
+        # If landing on a cell with 0 mines in neighboring cells
+        elif numbers[r][col] == 0:
+            vis = []
+            mine_values[r][col] = '0'
+            neighbours(r, col)
+ 
+        # If selecting a cell with atleast 1 mine in neighboring cells  
+        else:   
+            mine_values[r][col] = numbers[r][col]
+ 
+        # Check for game completion 
+        if(check_over()):
+            show_mines()
+            print_mines_layout()
+            print("Congratulations!!! YOU WIN")
+            over = True
+            continue
+        clear() 
